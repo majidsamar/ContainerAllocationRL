0bac5994028859c796c29e996574481eac55cbf2
MJ <majid.samar@gmail.com>
2025-07-18 09:43:28 +0200

    adding greedy and cplex

--- solvers/cplex_test.py
+++ solvers/cplex_test.py
@@ -1,5 +1,7 @@
 from docplex.mp.model import Model
 import numpy as np
+from datetime import datetime
+
 # m = Model(name='test')
 # x = m.binary_var(name='x')
 # y = m.binary_var(name='y')
@@ -28,95 +30,118 @@ import numpy as np
 
 
 
-# Problem parameters
-num_containers = 9
-num_stacks = 3
-stack_height = 3
-
-np.random.seed(42)
-# dwell_times = np.random.randint(1, 21, size=num_containers)
-dwell_times = [2,1,3,8,5,4,3,7,6]
-print("Dwell times:", dwell_times)
-
-# Index sets
-C = range(num_containers)
-S = range(num_stacks)
-T = range(stack_height)
-
-# Model
-mdl = Model("container_stacking_fifo")
-
-# Binary decision variables:
-# x[c,s,t] = 1 if container c is placed at stack s, tier t
-x = {(c, s, t): mdl.binary_var(name=f"x_{c}_{s}_{t}") for c in C for s in S for t in T} # 30 * 6 * 5 = 900 only for this
-
-# Constraint 1: each container must be assigned to one and only one position
-for c in C:
-    mdl.add_constraint(mdl.sum(x[c, s, t] for s in S for t in T) == 1)
-    # container c can not be located in two places
-
-# constraint 2: maximum one container per position
-for s in S:
-    for t in T:
-        mdl.add_constraint(mdl.sum(x[c, s, t] for c in C) <= 1)
-
-#constraint 3: keeping stacking order (no floating on the air container)
-for s in S:
-    for t in range(1, stack_height):
-        # here sum applies on many zero and only one 1 : [0,0,0,1,0,0]
-        mdl.add_constraint(
-            mdl.sum(x[c, s, t] for c in C) <= mdl.sum(x[c, s, t-1] for c in C)
-        )
-
-# constraint 4:  FIFO ordering
-for s in S:
-    for t1 in range(stack_height):
-        for t2 in range(t1 + 1, stack_height):
-            for i in C:
-                for j in C:
-                    if j > i:
-                        mdl.add_constraint(x[j, s, t1] + x[i, s, t2] <= 1)
-
-# Define violation variables: v[c1, c2] = 1 if c1 is above c2 and has higher dwell
-violations = []
-for s in S:
-    for t1 in range(1, stack_height):
-        for t2 in range(t1):
-            for c1 in C:
-                for c2 in C:
-                    if dwell_times[c1] >= dwell_times[c2]:
-                        v = mdl.binary_var(name=f"v_{c1}_{c2}_{s}_{t1}_{t2}")
-                        mdl.add_constraint(
-                            v >= x[c1, s, t1] + x[c2, s, t2] - 1
-                        )
-                        violations.append(v)
-
-# Objective: Minimize number of dwell time violations
-mdl.minimize(mdl.sum(violations))
-
-solution = mdl.solve(log_output=True)
 
 
-allocation = {}
-if solution:
-    print("optimal allocation with minimum violations:")
-    stacks = {s: [None] * stack_height for s in S}
+def run_solver(num_containers,num_stacks,stack_height,dwell_times,time_limit_sec):
+    C = range(num_containers)
+    S = range(num_stacks)
+    T = range(stack_height)
+
+    # Model
+    mdl = Model("container_stacking_fifo")
+    mdl.parameters.timelimit = time_limit_sec
+    # Binary decision variables:
+    # x[c,s,t] = 1 if container c is placed at stack s, tier t
+    x = {(c, s, t): mdl.binary_var(name=f"x_{c}_{s}_{t}") for c in C for s in S for t in T} # 30 * 6 * 5 = 900 only for this
+
+    # Constraint 1: each container must be assigned to one and only one position
     for c in C:
-        for s in S:
-            for t in T:
-                if x[c, s, t].solution_value > 0.5:
-                    stacks[s][t] = c
+        mdl.add_constraint(mdl.sum(x[c, s, t] for s in S for t in T) == 1)
+        # container c can not be located in two places
+
+    # constraint 2: maximum one container per position
+    for s in S:
+        for t in T:
+            mdl.add_constraint(mdl.sum(x[c, s, t] for c in C) <= 1)
+
+    #constraint 3: keeping stacking order (no floating on the air container)
+    for s in S:
+        for t in range(1, stack_height):
+            # here sum applies on many zero and only one 1 : [0,0,0,1,0,0]
+            mdl.add_constraint(
+                mdl.sum(x[c, s, t] for c in C) <= mdl.sum(x[c, s, t-1] for c in C)
+            )
+
+    # constraint 4:  FIFO ordering ,
+    # using the fact that:
+    # if j>i which means container j is after container i, and if t2 > t1 which means
+    # container i is higher than container j,
+    # then both can not ba in the same stack (if they are then x[j, s, t1] + x[i, s, t2] == 2 and FIFO is violated)
+    # this ensures that their stack is different
 
     for s in S:
-        print(f"\nStack {s}:")
-        for t in reversed(T):
-            c = stacks[s][t]
-            if c is not None:
-                print(f"  Tier {t}: Container {c} (Dwell {dwell_times[c]})")
-            else:
-                print(f"  Tier {t}: Empty")
+        for t1 in range(stack_height):
+            for t2 in range(t1 + 1, stack_height):
+                for i in C:
+                    for j in C:
+                        if j > i:
+                            mdl.add_constraint(x[j, s, t1] + x[i, s, t2] <= 1)
 
-    print("\nTotal dwell time violations:", int(mdl.objective_value))
-else:
-    print("No solution found.")
+    # defining violation variables: v[c1, c2] = 1 if c1 is above c2 and has higher dwell
+    violations = []
+    for s in S:
+        for t1 in range(1, stack_height):
+            for t2 in range(t1):
+            # for t2 in range(t1-1,t1):  # only consider one tier before c1
+                for c1 in C:
+                    for c2 in C:
+                        if dwell_times[c1] >= dwell_times[c2]:
+                            v = mdl.binary_var(name=f"v_{c1}_{c2}_{s}_{t1}_{t2}")
+                            mdl.add_constraint(
+                                v >= x[c1, s, t1] + x[c2, s, t2] - 1 # -1 to model logical and
+                                # c1 has higher dwell time that c2 but sits on t1 which is higher than t2
+                                # if both x[c1,s,t1] == 1 and x[c2,s,t2] == 1 --> then v must be 1
+                                # otherwise, v can be 0
+                            )
+                            violations.append(v)
+
+
+    mdl.minimize(mdl.sum(violations))
+    solution = mdl.solve(log_output=True)
+
+
+    allocation = {}
+    if solution:
+        print("optimal allocation with minimum violations:")
+        stacks = {s: [None] * stack_height for s in S}
+        for c in C:
+            for s in S:
+                for t in T:
+                    if x[c, s, t].solution_value > 0.5:
+                        stacks[s][t] = c
+
+        for s in S:
+            print(f"\nStack {s}:")
+            for t in reversed(T):
+                c = stacks[s][t]
+                if c is not None:
+                    print(f"  Tier {t}: Container {c} (Dwell {dwell_times[c]})")
+                else:
+                    print(f"  Tier {t}: Empty")
+
+        print("\nTotal dwell time violations:", int(mdl.objective_value))
+    else:
+        print("No solution found.")
+    print("Dwell times:", dwell_times)
+
+
+num_containers = 135
+num_stacks = 27
+stack_height = 5
+time_limit_sec = 300 # 5 min
+
+
+# np.random.seed(42)
+
+
+dwell_times = np.random.randint(1, 21, size=num_containers)
+# dwell_times = [2,1,3,8,5,4,3,7,6]
 print("Dwell times:", dwell_times)
+
+start_time = datetime.now()
+run_solver(num_containers=num_containers,
+           num_stacks=num_stacks,
+           stack_height=stack_height,
+           dwell_times=dwell_times,
+           time_limit_sec= time_limit_sec)
+print("\n\nDuration:", datetime.now() - start_time)
--- solvers/greedy_simple.py
+++ solvers/greedy_simple.py
@@ -49,6 +49,46 @@ def one_round(num_containers,bays,rows,tiers):
     return total_violations
 
 
+
+def individual_test():
+    bays= 4
+    rows=1
+    tiers=6
+    # dwell_times =[2, 1, 3, 8, 5, 4, 3, 7, 6]
+    dwell_times = [7, 20, 15, 11, 8, 7, 19, 11, 11, 4, 8, 3,
+                   2, 12, 6, 2, 1, 12, 12, 17, 10, 16, 15, 15]
+
+    print("Dwell Times (queue order):", dwell_times)
+
+    stacks = [[] for _ in range(bays* rows)]
+
+
+    total_violations = 0
+    for i, dwell in enumerate(dwell_times):
+        best_stack = None
+        min_violation = float('inf')
+
+        for s_idx, stack in enumerate(stacks):
+            if len(stack) < tiers:
+                vio = calculate_violations(stack, dwell)
+                if vio < min_violation:
+                    min_violation = vio
+                    best_stack = s_idx
+
+        if best_stack is not None:
+            total_violations += calculate_violations(stacks[best_stack], dwell)
+            stacks[best_stack].append(dwell)
+        else:
+            print(f"Container {i} with dwell {dwell} could not be placed (no space).")
+
+    print("\nFinal Stacks:")
+    for idx, s in enumerate(stacks):
+        print(f"Stack {idx}: {s}")
+
+    print("\nTotal Dwell Time Violations:", total_violations)
+    return total_violations
+
+
 def multiple_rounds(bays,rows,tiers,fullness_ratio):
     num_rounds = 1000
     num_containers = round(rows * bays * tiers * fullness_ratio)
@@ -60,8 +100,18 @@ def multiple_rounds(bays,rows,tiers,fullness_ratio):
           f"avg of violation:{sum(violation_list) / len(violation_list):.3f} ")
 
 
+# def run():
+#     for t in range(3,7):
+#         for b in range(200,201):
+#             multiple_rounds(bays=b,rows=1,tiers=t,fullness_ratio= 0.9)
+#         print("\n")
 
-for t in range(6,9):
-    for b in range(4,20):
+
+def run():
+    configurations = [[100,2],[67,3],[50,4],[40,5],[33,6],[28,7],[25,8],[22,9],[20,10]]
+    for b,t in configurations:
         multiple_rounds(bays=b,rows=1,tiers=t,fullness_ratio= 1)
-    print("\n")
+        print("\n")
+
+run()
+# individual_test()
